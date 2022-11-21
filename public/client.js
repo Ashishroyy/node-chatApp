cf3d21e3712668c3842c6105eea0378f7461aa2f
Ashish  kumar <104258722+Ashishroyy@users.noreply.github.com>
2022-11-21 23:22:49 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,52 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea');
+let chatArea = document.querySelector('.chat-section');
+
+do{
+    name = prompt('enter your username: ')
+}while(!name)
+
+textarea.addEventListener('keyup', (event)=>{
+    if(event.key === 'Enter'){
+        sendMessage(event.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: name,
+        message: message.trim() 
+    }
+    // append
+    appendMessage(msg, 'outgoing')
+    textarea.value = '';
+    scrollTotop()
+
+// send to server
+  socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let maindiv = document.createElement('div')
+    let className = type;
+    maindiv.classList.add(className, 'msg')
+
+    let markup = `
+    <h1>${msg.user}</h1>
+    <p>${msg.message}</p>
+    `
+    maindiv.innerHTML = markup
+    chatArea.appendChild(maindiv)
+}
+// recieving msg
+ socket.on('message', (msg) =>{
+    appendMessage(msg , 'incoming')
+    scrollTotop()
+ }) 
+
+ function scrollTotop(){
+    chatArea.scrollTop = chatArea.scrollHeight; 
+ }
+
+ 
